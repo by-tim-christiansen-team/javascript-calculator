d558c0d479f7d246975b581c70575f762f156ffc
Tim Christiansen <tim-netz@web.de>
2017-12-14 23:15:37 +0100

    first script

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  // declare var
+
+    var input = 0;
+    var input_group = [];
+
+  // declare functions
+
+  function update(){
+
+  }
+
+  function clear(){
+
+  }
+
+  function clear_all(){
+
+  }
+
+  function calculate(){
+      // declare var
+      var result = input_group[0];
+      var length = input_group.length;
+
+      // remove first item
+      input_group.splice(0, 1);
+
+      // for loop to calculate
+  }
+
+  // button.click
+
+  $("button").click(function(){
+
+     alert(!this.value);
+ });
+});
